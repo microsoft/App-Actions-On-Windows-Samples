091bfff46cb62a14bf781312218197ce148f4497
Chase Slattery (He/Him) <cslattery@microsoft.com>
2025-05-29 23:30:16 -0700

    Added sample base for experimental entities, and added action samples for Table and Contact entity

---
+++ samples/com/cs/experimental/ExperimentalApp/ExperimentalApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shmuelie.WinRTServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shmuelie.WinRTServer.CsWinRT;
+
+namespace ExperimentalApp
+{
+    public static class Program
+    {
+        [global::System.STAThreadAttribute]
+        static async Task Main(string[] args)
+        {
+            global::WinRT.ComWrappersSupport.InitializeComWrappers();
+
+            await using (ComServer server = new())
+            {
+                ExperimentalActionProvider sampleActionProvider = new ExperimentalActionProvider();
+                server.RegisterClass<ExperimentalActionProvider, Windows.AI.Actions.Provider.IActionProvider>(() => sampleActionProvider);
+                server.Start();
+
+                global::Microsoft.UI.Xaml.Application.Start((p) => {
+                    var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+                    global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+                    new App();
+                });
+
+                // Regular disposing of the ComServer is broken (https://github.com/shmuelie/Shmuelie.WinRTServer/issues/28).
+                // We instead call the UnsafeDispose method to dispose it.
+                server.UnsafeDispose();
+            }
+        }
+    }
+}
